72f19e85c91152dbd574a677de1d7e43d28557ad
EderJulian <julianeder97@gmail.com>
2023-10-09 20:00:43 +0200

    objetos

---
+++ index.js
@@ -0,0 +1,73 @@
+const coche = {
+    marca: 'audi',
+    modelo: 'a1',
+    matricula: 'UwU'
+}
+
+const casa = {
+    codigopostal: '1111',
+    calle: 'ruedelpercebe',
+    portal: '2',
+    piso: 'izquierda'
+}
+
+const fullStackDeveloper = {
+    lenguajes: [],
+    proyectos: [],
+    
+}
+
+const perro = {
+    nombre: 'chibi',
+    raza: 'chihuahua',
+    color: 'blanco',
+    edad: 33,
+    ladrar: function () {
+
+        return 'guau'
+        
+    },
+
+    popo: function() {
+        return Math.random()*3;
+    
+    }
+
+}
+
+    
+
+
+const portatil = {
+     marca: 'lenovo'
+        
+
+}
+
+const marcaportatil = portatil.marca;
+
+
+const portatil2 = {
+    marca: 'razer'
+
+}
+const marcaportatil2 = portatil2['razer'];
+
+const concierto = {
+    grupos: [ 'grupo1', 'grupo2']
+}
+
+const grupos = concierto['grupos'];
+
+
+
+const Led = {
+    rojo: false,
+    verde: false,
+    azul: true,
+};
+const arrayRGB = [Led.rojo, Led.verde, Led.azul]
+//9
+// const Portatil3 = {
+//     modelo: "nombre"
+// };
